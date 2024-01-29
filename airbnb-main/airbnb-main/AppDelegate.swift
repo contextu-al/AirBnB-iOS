27a141fd15fb45816d6773bd633e8440dd6be60a
Marc Stroebel <mdstroebel@gmail.com>
2024-01-29 15:58:27 +1100

    feature: set up and tested the single repo for all iOS GuideBlocks

--- airbnb-main/airbnb-main/AppDelegate.swift
+++ airbnb-main/airbnb-main/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import AppCenter
 import AppCenterDistribute
 import ContextualSDK
-import MultiSelectSurvey
+import iOS_GuideBlocks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
